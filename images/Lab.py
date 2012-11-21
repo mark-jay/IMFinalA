2689603e3f21201a1071b4a74ca0f7772985bcdd
unknown <fallen@.(none)>
2012-11-21 03:10:07 +0000

    all of the coins is visible now

--- images/Lab.py
+++ images/Lab.py
@@ -17,8 +17,8 @@ allImages = ["P1000697s.jpg", "P1000698s.jpg", "P1000697s.jpg",
             "P1000706s.jpg", "P1000709s.jpg", "P1000710s.jpg", "P1000713s.jpg"]
 
 #another way: im_gray = cv2.imread('grayscale_image.png', cv2.CV_LOAD_IMAGE_GRAYSCALE)
-def mycvtConvert(im):
-    return cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+def mycvtConvert(color = cv2.COLOR_BGR2GRAY):
+    return lambda im : cv2.cvtColor(im, color)
 
 def myPrint(im):
     winName = '__'
@@ -29,7 +29,7 @@ def myPrint(im):
 
 def myThreshold(ims):
     (treshold, _) = cv2.threshold(ims, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    return cv2.threshold(ims, treshold-10, 255, cv2.THRESH_BINARY)[1]
+    return cv2.threshold(ims, treshold, 255, cv2.THRESH_BINARY)[1]
 
 def getKernel(n = 7):
     return cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (n,n))
@@ -44,11 +44,20 @@ def myContours1(im):
                             hierarchy = hierarchy1)
     return im
 
-def closeMO(im, kernel = getKernel()):
-    return cv2.erode(cv2.dilate(im, kernel), kernel)
+def dilate(kernel = getKernel()):
+    return lambda img : cv2.dilate(img, kernel)
 
-def openMO(im, kernel = getKernel()):
-    return cv2.dilate(cv2.erode(i, kernel), kernel)
+def erode(kernel = getKernel()):
+    return lambda img : cv2.erode(img, kernel)
+
+def closeMO(kernel = getKernel(), iterations = 1):
+    return lambda im: cv2.morphologyEx(im, cv2.MORPH_OPEN, kernel, 
+                                       iterations = iterations)
+    #cv2.erode(cv2.dilate(im, kernel), kernel)
+
+def openMO(kernel = getKernel(), iterations = 1):
+    return lambda im: cv2.morphologyEx(im, cv2.MORPH_OPEN, kernel, 
+                                       iterations = iterations)
 
 def myContours(im):
     im = np.array(im)
@@ -57,58 +66,53 @@ def myContours(im):
     # myPrint(contours)
     cv2.drawContours(im, contours, -1, 150,  hierarchy = hierarchy1)
     return im
-
-def run():
-    # im4 = myContours(im3)
-    #cv2.dilate
-    #cv2.erode
-    im3 = map(comp([myThreshold, mycvtConvert, cv2.imread]), allImages)
-    print im3
-    #print (im3 == im4) # error - why?
-    #myPrint(im3)
     
-    # myPrint(closeMO(im4, getKernel(10)))
-    # im4 = map(closeMO, im3)
-    #im5 = openMO(im3, getKernel(2))
-    map(myPrint, im3)
-    # myPrint(im4)
+# n must be 0, 1 or 2
+def splitFn(n): 
+    def f(im):
+        return cv2.split(im)[n]
+    return f
+
+# has a side effect: write the answer to the second arr
+def sumMask(m1, m2):
+    """
+    for i in range(len(m1)):
+        for j in range(len(m1[i])):
+            # if (m1[i][j] > 0) | (m2[i][j] > 0):
+                # m2[i][j] = 255
+            # m1[i][j] = 100
+            if (m1[i][j] > 0):
+                pass
+                #m2[i][j] = 255
+    """
+    return m1 + m2
+
+def mkMaskCombinator(mFn1, mFn2):
+    return lambda im : sumMask(mFn1(im), mFn2(im))
+
+# 8, 5, 8, 5, 4, 4, 5, 3, 3, 3, 
+def run():
+    n = 10
+    def printImg(f, imgs):
+        map(comp([myPrint, f, cv2.imread]), imgs)
+
+    generalContoursFn = comp([myContours, myThreshold, mycvtConvert()])
+    defaultMaskFn = comp([closeMO(getKernel(n)), myThreshold, mycvtConvert()])
+    # redMaskFn = comp([myPrint, closeMO(), myThreshold, mycvtConvert(), cv2.imread])
+    redMaskFn = comp([closeMO(getKernel(n)), myThreshold, splitFn(2)]) # 2 is red
+    
+    maskFn = comp([erode(), erode(), erode(), mkMaskCombinator(defaultMaskFn, redMaskFn)])
+    
+    printImg(lambda x:x, allImages[:])
+    printImg(maskFn, allImages[:])
+    
+    # printImg(redMaskFn, allImages[3:4])
+    
+    # printImg(generalContoursFn, allImages[:1])
+    # printImg(maskFn, allImages[3:4])
+    # printImg(redMaskFn, allImages[3:4])
+    
+    # printImg(generalFn, allImages[:1])
+
 
 run()
-
-"""
-im1 = myRead1()
-im2 = mycvtConvert(im1)
-im3 = myThreshold(im2)
-im4 = myContours(im3)
-
-
-myPrint(im4)
-
-----------------------------------------------------------
-
-def getKernel():
-    #variant 0
-   # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-    #variant 1
-    #y,x = np.ogrid[-3: 3+1, -3: 3+1]
-    #mask = x**2+y**2 <= 3**2
-    #kernel= array(map(lambda b:map(lambda a: (1 if a else 0), b), mask))   
-    #variant 2
-    #kernel = np.ones((7,7),'int')
-    #variant 3    
-    #kernel = np.array([[0,0,0,1,0,0,0],[0,1,1,1,1,1,0],[0,1,1,1,1,1,0],[1,1,1,1,1,1,1],[0,1,1,1,1,1,0],[0,1,1,1,1,1,0],[0,0,0,1,0,0,0]],'int')
-    #variant 4
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7,7))
-    return kernel
-
-def myBI2(im):
-#    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7))
-#    res = cv2.morphologyEx(im,cv2.MORPH_OPEN,kernel)
-    des = cv2.bitwise_not(getKernel())
-    contour,hier = cv2.findContours(des,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contour:
-        cv2.drawContours(des,[cnt],0,255,-1)
-    im = cv2.bitwise_not(des)
-    return im
-
-"""
